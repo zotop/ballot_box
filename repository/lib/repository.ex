64c2e8be2e399253b3acca563712e86b4f390ee2
Joao Guerreiro <jmdguerreiro@gmail.com>
2018-03-21 21:42:43 +0100

    added comment for future work

--- repository/lib/repository.ex
+++ repository/lib/repository.ex
@@ -2,7 +2,7 @@ defmodule Repository do
   @moduledoc """
   Documentation for Repository.
   """
-  
+
   def create_question(question, answers) do
     answers = Enum.map(answers, fn(answer) -> %{answer: answer, votes: 0} end)
     question = Ecto.Changeset.change(%Repository.Question{}, question: question, answer: answers)
@@ -13,6 +13,7 @@ defmodule Repository do
     Voting.Repo.insert!(%Repository.Voter{})
   end
 
+  #TODO: a voter should not be able to vote twice in the same question
   def vote!(voter_id, answer_id) do
     voter = Voting.Repo.get_by!(Repository.Voter, id: voter_id)
     answer_to_vote_for = Voting.Repo.get_by!(Repository.Answer, id: answer_id)
