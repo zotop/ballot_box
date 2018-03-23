cf16ae54c3d291514863276a0bd1548dba778a3c
João Guerreiro <joao.guerreiro@valtech.se>
2018-03-23 15:51:39 +0100

    translated remaining tests to espec

--- repository/lib/repository.ex
+++ repository/lib/repository.ex
@@ -17,7 +17,7 @@ defmodule Repository do
   end
 
   # TODO: a voter should not be able to vote twice in the same question
-  def vote!(voter_id, answer_id) do
+  def vote(voter_id, answer_id) do
     Voting.Repo.get_by!(Repository.Voters, id: voter_id)
     answer_to_vote_for = Voting.Repo.get_by!(Repository.Answers, id: answer_id)
 
--- repository/spec/repository_spec.exs
+++ repository/spec/repository_spec.exs
@@ -1,4 +1,5 @@
 defmodule RepositorySpec do
+  import Ecto.Query, only: [from: 2]
   use ESpec
 
   before_all do
@@ -25,6 +26,77 @@ defmodule RepositorySpec do
       [answer_1, answer_2] = created_answers
 
       expect([answer_1.votes, answer_2.votes]).to eq([0, 0])
+    end
+
+  end
+
+  context "when creating a voter" do
+
+    let! created_voter: Repository.create_voter()
+
+    it "should generate an UUID" do
+      number_of_voters = Voting.Repo.aggregate(Repository.Voters, :count, :id)
+      voter_id = Voting.Repo.one(from(x in Repository.Voters, select: x.id))
+
+      expect(number_of_voters).to be(1)
+      expect(created_voter.id).to eq(voter_id)
+    end
+
+  end
+
+  context "when voting for an answer" do
+
+    let question: "What is my name?"
+    let answers: ["John", "Julia"]
+    let! created_question: Repository.create_question(question, answers)
+    let answer_id: Enum.at(created_question.answers, 0).id
+
+    context "and voter is not registered" do
+
+      it "should raise an error and not " do
+        voting = fn -> Repository.vote(Ecto.UUID.generate(), answer_id) end
+
+        expect voting |> to(raise_exception())
+      end
+
+      it "should not increment the votes for the answer" do
+        try do
+          Repository.vote(Ecto.UUID.generate(), answer_id)
+        rescue e ->
+          vote_count =
+            Voting.Repo.one(
+              from(
+                x in Repository.Answers,
+                where: x.id == ^answer_id,
+                select: x.votes
+              )
+            )
+          expect(vote_count).to be(0)
+        end
+      end
+
+    end
+
+    context "with valid voters" do
+
+      let! voter_1: Repository.create_voter()
+      let! voter_2: Repository.create_voter()
+
+      it "should increment the number of votes for the answer" do
+        Repository.vote(voter_1.id, answer_id)
+        Repository.vote(voter_2.id, answer_id)
+        vote_count =
+          Voting.Repo.one(
+            from(
+              x in Repository.Answers,
+              where: x.id == ^answer_id,
+              select: x.votes
+            )
+          )
+
+        expect(vote_count).to be(2)
+      end
+
     end
 
   end
