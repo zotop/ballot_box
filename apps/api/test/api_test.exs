da8755908787c5ef06a65d363c26c85d989b6626
Joao Guerreiro <jmdguerreiro@gmail.com>
2018-03-24 18:55:18 +0100

    sample test

--- apps/api/test/api_test.exs
+++ apps/api/test/api_test.exs
@@ -1,8 +1,19 @@
 defmodule ApiTest do
-  use ExUnit.Case
-  doctest Api
+  use ExUnit.Case, async: true
+  use Plug.Test
 
-  test "greets the world" do
-    assert Api.hello() == :world
+  @opts Api.init([])
+
+  test "returns hello world" do
+    # Create a test connection
+    conn = conn(:get, "/hello")
+
+    # Invoke the plug
+    conn = Api.call(conn, @opts)
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "world"
   end
 end
