a344fa1b5aa91b4b6c5f0518a70a7991aff8172a
Joao Guerreiro <jmdguerreiro@gmail.com>
2018-03-27 21:29:54 +0200

    serving static file in root path

--- apps/api/lib/api.ex
+++ apps/api/lib/api.ex
@@ -2,11 +2,17 @@ defmodule Api do
   require Repository
   use Plug.Router
 
+  plug Plug.Static, at: "/", from: :api
   plug :match
   plug Plug.Parsers, parsers: [:json],
                      pass:  ["application/json"],
                      json_decoder: Jason
   plug :dispatch
+
+  get "/" do
+    conn = put_resp_content_type(conn, "text/html")
+    send_file(conn, 200, "priv/static/index.html")
+  end
 
   post "/question" do
      json = conn.body_params
