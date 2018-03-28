a8d1849b3c70bc0290f52427542d1e690672e9d2
Joao Guerreiro <jmdguerreiro@gmail.com>
2018-03-28 23:01:27 +0200

    map of different pages to render. added jquery

--- apps/api/priv/static/main.js
+++ apps/api/priv/static/main.js
@@ -1,23 +1,56 @@
 $(function () {
 
-  function render(url) {
+  hideAllPages();
+  render("/#/");
 
+  function render(url) {
+    var temp = url.split("/#/");
+    temp = temp[1];
+
+    var map = {
+
+      '': function() {
+              hideAllPages();
+              renderQuestionListPage();
+          },
+
+      'questions/new': function() {
+              hideAllPages();
+              renderCreateNewQuestionPage();
+          }
+
+    };
+
+    if(map[temp]){
+      map[temp]();
+    }
   }
 
   function renderQuestionListPage(data) {
-
+    var page = $('#question-list-page');
+    page.css("display", "block");
   }
 
   function renderCreateNewQuestionPage(data) {
-
+    var page = $('#create-new-question-page');
+    page.css("display", "block");
   }
 
   function renderQuestionVotingPage(data) {
-
+    var page = $('#question-voting-page');
+    page.css("display", "block");
   }
 
   function renderQuestionResultsPage(data) {
+    var page = $('#question-results-page');
+    page.css("display", "block");
+  }
 
+  function hideAllPages() {
+    $('#question-list-page').css("display", "none");
+    $('#create-new-question-page').css("display", "none");
+    $('#question-voting-page').css("display", "none");
+    $('#question-results-page').css("display", "none");
   }
 
 });
