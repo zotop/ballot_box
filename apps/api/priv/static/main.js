11adf5b6386b2422aee04a21a998e6b5a82ecb16
Joao Guerreiro <jmdguerreiro@gmail.com>
2018-04-03 23:45:10 +0200

    switching to horizontal bar chart

--- apps/api/priv/static/main.js
+++ apps/api/priv/static/main.js
@@ -151,21 +151,16 @@ $(function () {
 
   function pieData() {
     var data = {
-      type: 'pie',
+      type: 'horizontalBar',
       data: {
-        labels: ["M", "T", "W", "T", "F", "S", "S"],
+        labels: ["Answer 1", "Answer 2", "Answer 3", "T", "F", "S", "S"],
         datasets: [{
-          backgroundColor: [
-            "#2ecc71",
-            "#3498db",
-            "#95a5a6",
-            "#9b59b6",
-            "#f1c40f",
-            "#e74c3c",
-            "#34495e"
-          ],
           data: [12, 19, 3, 17, 28, 24, 7]
         }]
+      },
+      options: {
+         responsive: true,
+         maintainAspectRatio: false
       }
     }
     return data;
